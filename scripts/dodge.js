068799d51dba9240dfee561f23efd61afdc72e04
marl0stanfield <96742086+marl0stanfield@users.noreply.github.com>
2024-07-31 05:25:58 +0200

    Update dodge.js

--- scripts/dodge.js
+++ scripts/dodge.js
@@ -333,7 +333,7 @@ class AttackDodger {
       console.log("Attack count: " + this.attackCount);
     }
 
-    const mutations
+    const mutations =;
     const mutationObserver = new MutationObserver((mutations) => {
       setTimeout(() => {
         if (mutations[2].type === "childList") {
